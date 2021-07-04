2754e0a39fe876b31137ae80cc87a94da9a177a9
Ryan Hinkle <ryan.hinkle@integral.io>
2021-07-03 22:33:55 -0400

    Create an inMemoryDB Base

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,36 @@
-const hello: string = "world"
+console.log("============== New Run ==============")
+interface Cats {
+    id: string;
+    hitPoints: number;
+    attack: number;
+}
 
-console.log(hello);
+interface BaseRecord {
+    id: string;
+}
+
+interface Database<T extends BaseRecord> {
+    set(newValue: T): void;
+    get(id: string): T | undefined;
+}
+
+class InMemoryDatabase<T extends BaseRecord> implements Database<T> {
+    private db: Record<string, T> = {}
+
+    get(id: string): T | undefined {
+        return this.db[id];
+    }
+
+    set(newValue: T): void {
+        this.db[newValue.id] = newValue;
+    }
+}
+
+const bagOfCats = new InMemoryDatabase<Cats>();
+
+bagOfCats.set({
+    id: "1234",
+    hitPoints: 100,
+    attack: 15
+});
+console.log(bagOfCats.get("1234"));
